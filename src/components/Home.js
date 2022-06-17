ccd877afce9e799a683f30edbfd35c9feb325788
sselvapriyaa <selvapriyaphd@gmail.com>
2022-06-17 06:35:39 -0500

    Selva: updated Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ import {
   NavDropdown,
   Row,
 } from "react-bootstrap";
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Link, Routes, Route } from "react-router-dom";
 // Personal Style
 import "../App.css";
 
